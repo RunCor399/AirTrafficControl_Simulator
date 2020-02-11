3f04be2f650cab8054e10e65a6feda5f7c754b6c
Manuel Colotti <manuel.colotti@studio.unibo.it>
2020-02-11 18:05:22 +0100

    First project setup

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+// Declaration of the Gradle extension to use
+plugins {
+    java
+    application
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+}
+repositories {
+    jcenter() // Contains the whole Maven Central
+}
+// List of JavaFX modules
+val javaFXModules = listOf(
+    "base",
+    "controls",
+    "fxml",
+    "swing",
+    "graphics"
+)
+// All required for OOP
+val supportedPlatforms = listOf("linux", "mac", "win")
+
+dependencies {
+    //JavaFX dependency
+    for (platform in supportedPlatforms) {
+        for (module in javaFXModules) {
+            implementation("org.openjfx:javafx-$module:13:$platform")
+        }
+    }
+    // JUnit API and testing engine
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+}
+
+tasks.withType<Test> {
+    // Enables JUnit 5 Jupiter module
+    useJUnitPlatform()
+}
+
+application {
+    mainClassName = "application.Launcher"
+}
