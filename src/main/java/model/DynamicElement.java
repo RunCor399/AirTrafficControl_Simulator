8d4ed1aadc5bccadb27a1de1c100a40d3fb419f3
William Carletti <william.carletti@studio.unibo.it>
2020-02-15 15:45:13 +0100

    First implementation of DynamicElement interface

---
+++ src/main/java/model/DynamicElement.java
@@ -0,0 +1,37 @@
+package model;
+
+/**
+ * 
+ * An interface that models a radar element which is able to move.
+ *
+ */
+public interface DynamicElement /* implements RadarElememt */ {
+
+    /**
+     * Method that returns the actual altitude.
+     * 
+     * @return the actual altitude.
+     */
+    double getAltitude();
+
+    /**
+     * Method that returns the actual speed.
+     * 
+     * @return the actual altitude.
+     */
+    Speed getSpeed();
+
+    /**
+     * Method that returns the actual direction.
+     * 
+     * @return the actual direction.
+     */
+    double getDirection();
+
+    /**
+     * This method computes the new position, speed, altitude and direction of the
+     * element after a time quantum.
+     */
+    void computeNewPosition();
+
+}
