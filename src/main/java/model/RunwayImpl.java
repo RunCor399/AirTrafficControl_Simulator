61c27b1d7bb3c70960fab6b704130e00b4a66983
marco.lisotta <marco.lisotta@studio.unibo.it>
2020-03-03 12:10:22 +0100

    Implemented methods and created new private method for check runwayEnd

--- src/main/java/model/RunwayImpl.java
+++ src/main/java/model/RunwayImpl.java
@@ -6,11 +6,11 @@ public class RunwayImpl implements Runway {
 
     private final Pair<RunwayEnd, RunwayEnd> runwayends;
 
-    public RunwayImpl(final String end1, final String end2) {
+    public RunwayImpl(final String end1, final RadarPosition endPosition1, final String end2, final RadarPosition endPosition2) {
         Objects.requireNonNull(end1);
         Objects.requireNonNull(end2);
 
-        this.runwayends = new Pair<>(new RunwayEndImpl(end1), new RunwayEndImpl(end2));
+        this.runwayends = new Pair<>(new RunwayEndImpl(end1, endPosition1), new RunwayEndImpl(end2, endPosition2));
     }
 
     /**
@@ -18,7 +18,7 @@ public class RunwayImpl implements Runway {
      */
     @Override
     public String getRunwayStatus() {
-        return this.runwayends.getX().getStatus() ? this.runwayends.getX().toString() : this.runwayends.getY().toString();
+        return this.runwayends.getX().getStatus() ? this.runwayends.getX().getNumRunwayEnd() : this.runwayends.getY().getNumRunwayEnd();
     }
 
     /**
@@ -26,7 +26,7 @@ public class RunwayImpl implements Runway {
      */
     @Override
     public Pair<RadarPosition, RadarPosition> getPosition() {
-        return null;
+        return new Pair<>(this.runwayends.getX().getPosition(), this.runwayends.getY().getPosition());
     }
 
     /**
@@ -43,7 +43,13 @@ public class RunwayImpl implements Runway {
      */
     @Override
     public void setActiveRunwayEnd(final String numRunwayEnd) {
-        // TODO Auto-generated method stub
+        checkRunwayEnd(numRunwayEnd);
+        if (Integer.parseInt(numRunwayEnd) == Integer.parseInt(this.runwayends.getX().getNumRunwayEnd())) {
+            this.runwayends.getX().changeStatus(true);
+            this.runwayends.getY().changeStatus(false);
+        }
+        this.runwayends.getX().changeStatus(false);
+        this.runwayends.getY().changeStatus(true);
     }
 
     /**
@@ -51,8 +57,19 @@ public class RunwayImpl implements Runway {
      */
     @Override
     public boolean isRunwayEndActive(final String numRunwayEnd) {
-        // TODO Auto-generated method stub
-        return false;
+        checkRunwayEnd(numRunwayEnd);
+        return Integer.parseInt(numRunwayEnd) == Integer.parseInt(this.runwayends.getX().getNumRunwayEnd()) ? this.runwayends.getX().getStatus() : this.runwayends.getY().getStatus();
+    }
+
+    /**
+     * 
+     */
+    private void checkRunwayEnd(final String numRunwayEnd) {
+        Objects.requireNonNull(numRunwayEnd);
+        int num = Integer.parseInt(numRunwayEnd);
+        if ((num != Integer.parseInt(this.runwayends.getX().getNumRunwayEnd())) || num != Integer.parseInt(this.runwayends.getY().getNumRunwayEnd())) {
+            throw new IllegalArgumentException("Not a runwayEnd");
+        }
     }
 
 }
