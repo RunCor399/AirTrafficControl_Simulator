266551297518e3cf48421765e0d62f249f20e46c
William Carletti <william.carletti@studio.unibo.it>
2020-03-06 20:37:25 +0100

    Minor additions to PlaneImpl

--- src/main/java/model/PlaneImpl.java
+++ src/main/java/model/PlaneImpl.java
@@ -6,11 +6,20 @@ import model.exceptions.RunwayNotAvailableException;
 
 /**
  * 
- * Implementation of a {@link Plane}, which is by definition a {@link DynamicElement}. 
+ * Implementation of a {@link Plane}, which is by definition a
+ * {@link DynamicElement}.
  *
  */
 public class PlaneImpl extends AbstractDynamicElement implements Plane {
 
+    /**
+     * The maximum altitude that allows the plane to land.
+     */
+    private static final double ALTITUDE_TO_LAND = 300;
+    /**
+     * The maximum speed that allows the plane to land.
+     */
+    private static final Speed SPEED_TO_LAND = new SpeedImpl(100.0);
     private static final Direction DIRECTION_DELTA = new DirectionImpl(1.8);
     private static final Speed SPEED_DELTA = new SpeedImpl(20.0);
     private static final double ALTITUDE_DELTA = 10;
@@ -52,8 +61,14 @@ public class PlaneImpl extends AbstractDynamicElement implements Plane {
      * {@inheritDoc}
      */
     @Override
-    public void land(/*final Airport airport*/) throws RunwayNotAvailableException {
-        // TODO Auto-generated method stub
+    public void land(/* final Airport airport */) throws RunwayNotAvailableException {
+        // I take an active runway. From this one, i get the active runwayEnd
+        RadarPosition runwayEndPosition = new RadarPositionImpl(new Position2DImpl(0.0, 0.0));
+        // I check if it's close enough and the parameters are correct (altitude, speed,
+        // direction)
+        if (this.getSpeed().getAsKnots() <= SPEED_TO_LAND.getAsKnots() && this.getAltitude() <= ALTITUDE_TO_LAND) {
+            // land
+        }
 
     }
 
@@ -61,8 +76,11 @@ public class PlaneImpl extends AbstractDynamicElement implements Plane {
      * {@inheritDoc}
      */
     @Override
-    public void takeOff(/*final Airport airport*/) throws RunwayNotAvailableException {
-        // TODO Auto-generated method stub
+    public void takeOff(/* final Airport airport */) throws RunwayNotAvailableException {
+        // I take an active runway. From this one, i get the active runwayEnd
+        // The other runwayEnd will be the target of the plane.
+        RadarPosition runwayEndPosition = new RadarPositionImpl(new Position2DImpl(0.0, 0.0));
+        this.setPosition(runwayEndPosition);
 
     }
 
