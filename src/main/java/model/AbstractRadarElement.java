cd0477f5e2ba20ff2f1ce74b1b11e938497332bc
Alessandro Angelini <alessandro.angelin10@studio.unibo.it>
2020-03-11 16:00:22 +0100

    Added serializable in AbstractRadarElement

--- src/main/java/model/AbstractRadarElement.java
+++ src/main/java/model/AbstractRadarElement.java
@@ -1,9 +1,14 @@
 package model;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public abstract class AbstractRadarElement implements RadarElement {
+public abstract class AbstractRadarElement implements RadarElement, Serializable  {
 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -7658350082008425580L;
     private RadarPosition position;
 
     /**
