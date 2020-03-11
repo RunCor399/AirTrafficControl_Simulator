aa2695c9c24df45247ce75376dd21043ea350386
Manuel Colotti <manuel.colotti@studio.unibo.it>
2020-03-11 15:22:05 +0100

    Restored previous Runway and RunwayEnd

--- src/main/java/model/RunwayEnd.java
+++ src/main/java/model/RunwayEnd.java
@@ -1,5 +1,30 @@
 package model;
 
-public interface RunwayEnd {
+/**
+ * 
+ * An interface that define the direction of use of a Runway.
+ *
+ */
+public interface RunwayEnd extends RadarElement {
 
+    /**
+     * This method change the status of a runwayEnd.
+     * 
+     * @param isActive Status of runwayEnd
+     */
+    void changeStatus(boolean isActive);
+
+    /**
+     * This method returns the status of the runwayEnd.
+     * 
+     * @return boolean Status of runwayEnd
+     */
+    boolean getStatus();
+
+    /**
+     * This method returns the number of the runwayEnd.
+     * 
+     * @return String Number of runwayEnd
+     */
+    String getNumRunwayEnd();
 }
--- src/main/java/model/RunwayImpl.java
+++ src/main/java/model/RunwayImpl.java
@@ -1,5 +1,58 @@
 package model;
 
-public class RunwayImpl {
+import java.util.Objects;
+
+public class RunwayImpl implements Runway {
+
+    private final Pair<RunwayEnd, RunwayEnd> runwayends;
+
+    public RunwayImpl(final String end1, final RadarPosition endPosition1, final String end2, final RadarPosition endPosition2) {
+        Objects.requireNonNull(end1);
+        Objects.requireNonNull(end2);
+
+        this.runwayends = new Pair<>(new RunwayEndImpl(end1, endPosition1), new RunwayEndImpl(end2, endPosition2));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public RunwayEnd getRunwayStatus() {
+        return this.runwayends.getX().getStatus() ? this.runwayends.getX() : this.runwayends.getY();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Pair<RadarPosition, RadarPosition> getPosition() {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setPosition(final Pair<RadarPosition, RadarPosition> positions) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setActiveRunwayEnd(final String numRunwayEnd) {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isRunwayEndActive(final String numRunwayEnd) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
 }
