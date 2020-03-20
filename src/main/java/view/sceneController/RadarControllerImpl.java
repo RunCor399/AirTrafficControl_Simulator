ee192ba6adf0ffc641c47490e249174e81b1b82f
William Carletti <william.carletti@studio.unibo.it>
2020-03-20 19:18:00 +0100

    Continued implementation of RadarControllerImpl

--- src/main/java/view/sceneController/RadarControllerImpl.java
+++ src/main/java/view/sceneController/RadarControllerImpl.java
@@ -2,23 +2,40 @@ package view.sceneController;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
 
 public class RadarControllerImpl extends AbstractSceneController {
 
+    private GraphicsContext radarContext;
     @FXML
     private Slider timeWarpSlider;
     @FXML
     private Button btnResume;
     @FXML
     private Button btnPause;
+    @FXML
+    private Canvas radarCanvas;
+    @FXML
+    private Pane radarPane;
 
     public void initialize() {
         this.timeWarpSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
             this.setTimeWarpValue((int) newValue);
         });
 
+    }
+
+    public void loadRadar() {
+        double parentWidth = this.radarPane.getWidth();
+        double parentHeight = this.radarPane.getHeight();
+        this.radarCanvas.setWidth(parentWidth);
+        this.radarCanvas.setHeight(parentHeight);
+        this.radarContext = this.radarCanvas.getGraphicsContext2D();
+        this.radarContext.translate(parentWidth / 2, parentHeight / 2);
     }
 
     private void setTimeWarpValue(final int value) {
