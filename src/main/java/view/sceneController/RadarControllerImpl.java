c991545882119a9760253e015c1c0125654350cf
William Carletti <william.carletti@studio.unibo.it>
2020-03-21 19:22:30 +0100

    Added methods in RadarController

--- src/main/java/view/sceneController/RadarControllerImpl.java
+++ src/main/java/view/sceneController/RadarControllerImpl.java
@@ -1,5 +1,6 @@
 package view.sceneController;
 
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
@@ -7,12 +8,14 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.Pane;
+import model.Airport;
+import model.Vor;
 
 public class RadarControllerImpl extends AbstractSceneController {
 
     private double xRatio;
     private double yRatio;
-    private GraphicsContext radarContext;
+
     @FXML
     private Slider timeWarpSlider;
     @FXML
@@ -21,6 +24,10 @@ public class RadarControllerImpl extends AbstractSceneController {
     private Button btnPause;
     @FXML
     private Canvas radarCanvas;
+    private GraphicsContext radarContext;
+    @FXML
+    private Canvas airportCanvas;
+    private GraphicsContext airportContext;
     @FXML
     private Pane radarPane;
 
@@ -32,7 +39,11 @@ public class RadarControllerImpl extends AbstractSceneController {
                 this.setTimeWarpValue(newValue.intValue());
             }
         });
-
+        ChangeListener<? super Number> resizeListener = (obs, oldVal, newVal) -> this.loadRadar();
+        this.radarPane.widthProperty().addListener(resizeListener);
+        this.radarPane.heightProperty().addListener(resizeListener);
+        this.radarContext = this.radarCanvas.getGraphicsContext2D();
+        this.airportContext = this.airportCanvas.getGraphicsContext2D();
     }
 
     public void loadRadar() {
@@ -40,13 +51,54 @@ public class RadarControllerImpl extends AbstractSceneController {
         double parentHeight = this.radarPane.getHeight();
         this.radarCanvas.setWidth(parentWidth);
         this.radarCanvas.setHeight(parentHeight);
-        this.radarContext = this.radarCanvas.getGraphicsContext2D();
-        this.radarContext.translate(parentWidth / 2, parentHeight / 2);
+        this.airportCanvas.setWidth(parentWidth);
+        this.airportCanvas.setHeight(parentHeight);
+    }
+
+    /**
+     * Method that computes the ratios used to obtain the coordinates of an object.
+     */
+    private void computeRatios() {
         // TODO cambia in valore del model
-        this.xRatio = parentWidth / (30000 * 2);
+        this.xRatio = this.radarCanvas.getWidth() / (30000 * 2);
         // Inverted the sign because of how the canvas considers the y axis.
-        this.yRatio = -parentHeight / (20000 * 2);
-        this.radarContext.fillText("TEST", 30000 * this.xRatio, 30000 * this.yRatio, 15);
+        this.yRatio = -this.radarCanvas.getHeight() / (20000 * 2);
+    }
+
+    /**
+     * Method that computes actual x coordinate of an object.
+     * 
+     * @param initX the x value of the object.
+     */
+    private double computeX(final double initX) {
+        return this.xRatio * initX + this.radarCanvas.getWidth() / 2;
+    }
+
+    /**
+     * Method that computes actual y coordinate of an object.
+     * 
+     * @param initY the y value of the object.
+     */
+    private double computeY(final double initY) {
+        return this.yRatio * initY + this.radarCanvas.getHeight() / 2;
+    }
+
+    /**
+     * Removes the planes of the radar.
+     */
+    private void clearRadar() {
+        this.radarContext.clearRect(0, 0, this.radarCanvas.getWidth(), this.radarCanvas.getHeight());
+    }
+
+    /**
+     * Method that draws the VORs and the Runways of the airplane.
+     * 
+     * @param airport the airport to draw.
+     */
+    private void drawAirport(final Airport airport) {
+        for (Vor vor : airport.getVorList().get()) {
+
+        }
     }
 
     public void drawPlanes(/* final List<Plane> planes */) {
