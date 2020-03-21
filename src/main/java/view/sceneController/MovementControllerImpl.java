a016ea7b4f90de06f729abddc4feb48c0c7791d2
Manuel Colotti <manuel.colotti@studio.unibo.it>
2020-03-21 18:59:50 +0100

    Moved in feature movementController class MovementControllerImpl

---
+++ src/main/java/view/sceneController/MovementControllerImpl.java
@@ -0,0 +1,36 @@
+package view.sceneController;
+
+import model.Direction;
+import model.Speed;
+import model.Vor;
+
+public class MovementControllerImpl extends AbstractSceneController implements SceneController {
+
+    public void setTargetAirplane(/*final Plane targetPlane*/) {
+
+    }
+
+    public void setCurrentSpeed(final Speed targetSpeed) {
+
+    }
+
+    public void setCurrentHeading(final Direction targetDirection) {
+
+    }
+
+    public void setCurrentAltitude(final double targetAltitude) {
+
+    }
+
+    public void executeTakeoff() {
+
+    }
+
+    public void executeLanding() {
+
+    }
+
+    public void headToVor(final Vor targetVor) {
+
+    }
+}
