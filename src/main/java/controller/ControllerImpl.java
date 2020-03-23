0e0bc020eb253ce8409004cc3b68cc91398d7c76
Manuel Colotti <manuel.colotti@studio.unibo.it>
2020-03-23 23:25:04 +0100

    Added first implementation of ControllerImpl

--- src/main/java/controller/ControllerImpl.java
+++ src/main/java/controller/ControllerImpl.java
@@ -1,9 +1,14 @@
 package controller;
 
+import java.util.Objects;
+
 import model.Direction;
+import model.Model;
+import model.ModelImpl;
 import model.Plane;
 import model.Speed;
 import model.Vor;
+import model.exceptions.OperationNotAvailableException;
 
 /**
  * 
@@ -11,52 +16,83 @@ import model.Vor;
  *
  */
 public class ControllerImpl implements Controller {
+    private Model model;
     private Plane currentSelectedPlane;
 
     public ControllerImpl() {
+        this.model = new ModelImpl();
         this.currentSelectedPlane = null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void selectTargetPlane(final int planeId) {
-        // TODO Auto-generated method stub
-
+        Objects.requireNonNull(planeId);
+        this.currentSelectedPlane = this.model.getPlaneById(planeId);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setPlaneSpeed(final Speed targetSpeed) {
-        // TODO Auto-generated method stub
-
+        Objects.requireNonNull(targetSpeed);
+        this.currentSelectedPlane.setTargetSpeed(targetSpeed);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setPlaneHeading(final Direction targetDirection) {
-        // TODO Auto-generated method stub
-
+        Objects.requireNonNull(targetDirection);
+        this.currentSelectedPlane.setTargetDirection(targetDirection);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setPlaneAltitude(final double targetAltitude) {
-        // TODO Auto-generated method stub
-
+        Objects.requireNonNull(targetAltitude);
+        this.currentSelectedPlane.setTargetAltitude(targetAltitude);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void goToVor(final Vor targetVor) {
-        // TODO Auto-generated method stub
-
+        Objects.requireNonNull(targetVor);
+        this.currentSelectedPlane.setTargetPosition(targetVor.getPosition());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void takeOff() {
-        // TODO Auto-generated method stub
-
+        try {
+            this.currentSelectedPlane.takeOff(this.model.getAirport());
+        } catch (OperationNotAvailableException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void land() {
-        // TODO Auto-generated method stub
-
+        try {
+            this.currentSelectedPlane.land(this.model.getAirport());
+        } catch (OperationNotAvailableException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 }
