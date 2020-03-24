02603ff53afcb975230001b995c35a0f7889f556
marco.lisotta <marco.lisotta@studio.unibo.it>
2020-03-24 12:28:15 +0100

    First instance of MenuController

---
+++ src/main/java/view/sceneController/MenuControllerImpl.java
@@ -0,0 +1,26 @@
+package view.sceneController;
+
+public class MenuControllerImpl extends AbstractSceneController implements SceneController {
+
+
+    /**
+     * Method that switches into game Scenery.
+     */
+    private void switchToGameScenery() {
+        //TODO
+    }
+
+    /**
+     * Method that switches into tutorial Scenery.
+     */
+    private void switchToTutorialScenery() {
+        //TODO
+    }
+
+    /**
+     * Method that leaves the game.
+     */
+    private void quitGame() {
+        //TODO
+    }
+}
