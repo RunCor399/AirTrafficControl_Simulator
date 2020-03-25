4f1016388e02933629dce8f01b2f21cb77977019
Manuel Colotti <manuel.colotti@studio.unibo.it>
2020-03-25 17:19:25 +0100

    Added layout in this branch and added FXML fields to movementController

--- src/main/java/view/sceneController/MovementControllerImpl.java
+++ src/main/java/view/sceneController/MovementControllerImpl.java
@@ -4,11 +4,74 @@ import java.util.Objects;
 
 import controller.Controller;
 import controller.ControllerImpl;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Spinner;
 import model.Direction;
 import model.Speed;
 import model.Vor;
 
 public class MovementControllerImpl extends AbstractSceneController implements SceneController {
+    @FXML
+    private Slider speedSlider;
+
+    @FXML
+    private Slider altitudeSlider;
+
+    @FXML
+    private Button takeoffButton;
+
+    @FXML
+    private Button landButton;
+
+    @FXML
+    private Label speedLabel;
+
+    @FXML
+    private Label altitudeLabel;
+
+    @FXML
+    private Spinner<Double> directionSpinner;
+
+    @FXML
+    private ChoiceBox<String> vorChoiceBox;
+
+    @FXML
+    public final void initialize() {
+
+        this.directionSpinner.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(final ObservableValue<? extends Number> obs, final Number oldValue,
+                    final Number newValue) {
+                System.out.println(newValue);
+            }
+        });
+
+        this.speedSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(final ObservableValue<? extends Number> obs, final Number oldValue,
+                    final Number newValue) {
+                speedLabel.textProperty().setValue(String.valueOf(newValue.intValue()));
+            }
+        });
+
+        this.altitudeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(final ObservableValue<? extends Number> obs, final Number oldValue,
+                    final Number newValue) {
+                altitudeLabel.textProperty().setValue(String.valueOf(newValue.intValue()));
+            }
+        });
+
+        this.speedLabel.setText("210");
+        this.altitudeLabel.setText("7000");
+    }
+
     private final Controller controller = new ControllerImpl();
 
     /**
@@ -52,20 +115,6 @@ public class MovementControllerImpl extends AbstractSceneController implements S
     }
 
     /**
-     * method that makes a plane takeoff from the airport.
-     */
-    public void executeTakeoff() {
-        this.controller.takeOff();
-    }
-
-    /**
-     * method that makes a plane land in a specific runway of the airport.
-     */
-    public void executeLanding() {
-        this.controller.land();
-    }
-
-    /**
      * method that directs a plane to a specific vor.
      * 
      * @param targetVor
@@ -74,4 +123,21 @@ public class MovementControllerImpl extends AbstractSceneController implements S
         Objects.requireNonNull(targetVor);
         this.controller.goToVor(targetVor);
     }
+
+    /**
+     * method that makes a plane takeoff from the airport.
+     */
+    @FXML
+    public void takeoffPressed() {
+        this.controller.takeOff();
+    }
+
+    /**
+     * method that makes a plane land in a specific runway of the airport.
+     */
+    @FXML
+    public void landPressed() {
+        this.controller.land();
+    }
+
 }
