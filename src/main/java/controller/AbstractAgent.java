9caa00efdeacc0ba642aa96b23160dcd11d4a79d
Manuel Colotti <manuel.colotti@studio.unibo.it>
2020-03-26 19:07:41 +0100

    Created new AbstractAgent class in order to merge common functionalities of several threads

---
+++ src/main/java/controller/AbstractAgent.java
@@ -0,0 +1,41 @@
+package controller;
+
+import model.Model;
+
+public abstract class AbstractAgent extends Thread{
+
+    private static final long DELTA_TIME = 500;
+    private static final int INITIAL_MULTIPLIER = 1;
+
+    private final Model model;
+    private volatile boolean stop;
+    private volatile boolean pause;
+    private volatile int multiplier;
+
+    public AbstractAgent(final Model model) {
+        this.model = model;
+        this.stop = false;
+        this.pause = false;
+        this.multiplier = INITIAL_MULTIPLIER;
+    }
+
+    /**
+     * method that takes a time multiplier as argument and sets it.
+     * 
+     * @param multiplier
+     */
+    public void setMultiplier(final int multiplier) {
+        if (multiplier < 1) {
+            throw new IllegalStateException();
+        }
+        this.multiplier = multiplier;
+    }
+
+    /**
+     * method that stops the thread.
+     */
+    public void stopThread() {
+        this.stop = true;
+        interrupt();
+    }
+}
