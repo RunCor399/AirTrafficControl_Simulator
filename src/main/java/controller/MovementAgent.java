fc4c33743d3023f140f3b388921c4cb0d6501d07
Manuel Colotti <manuel.colotti@studio.unibo.it>
2020-03-26 15:49:45 +0100

    modified view method param and started implementation of MovementAgent

---
+++ src/main/java/controller/MovementAgent.java
@@ -0,0 +1,70 @@
+package controller;
+
+import model.Model;
+import view.View;
+
+public class MovementAgent extends Thread {
+
+    private static final long DELTA_UPDATE = 500;
+
+    private final Model model;
+    private final View view;
+    private volatile boolean stop;
+    private volatile boolean pause;
+
+    public MovementAgent(final Model model, final View view) {
+        this.model = model;
+        this.view = view;
+        this.stop = false;
+        this.pause = false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void run() {
+        while (!this.stop) {
+            try {
+                synchronized (this) {
+                    if (this.pause) {
+                        this.wait();
+                    }
+                }
+              sleep(DELTA_UPDATE);
+            } catch (InterruptedException exception) {
+            }
+        }
+    }
+
+    private void updatePlanesPositionAndView() {
+        this.model.computeAllPlanePositions();
+        this.view.radarUpdate(this.model.getAllPlanes());
+    }
+
+    /**
+     * method that stops the thread.
+     */
+    public final void stopThread() {
+        this.stop = true;
+        interrupt();
+    }
+
+    /**
+     * method that resumes a thread currently in pause.
+     */
+    public synchronized void resumeThread() {
+        if (this.pause) {
+            this.pause = false;
+            this.notify();
+        }
+    }
+
+    /**
+     * method that pauses the thread.
+     */
+    public final void pauseThread() {
+        this.pause = true;
+        interrupt();
+    }
+}
