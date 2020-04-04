9351f304072fa6b4787a07bdd906bbfdf50ca073
Manuel Colotti <manuel.colotti@studio.unibo.it>
2020-04-04 13:52:12 +0200

    Deleted StripCreator for now

--- src/main/java/view/StripImpl.java
+++ src/main/java/view/StripImpl.java
@@ -1,16 +1,29 @@
 package view;
 
-import javafx.scene.layout.Pane;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import model.Plane;
 
-public class StripImpl extends Pane implements Strip {
+public class StripImpl extends Parent implements Strip {
+    private final Plane plane;
+    private int width;
+    private int height;
+
+    public StripImpl(final int width, final int height, final Plane plane) {
+        this.width = width;
+        this.height = height;
+        this.plane = plane;
+    }
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void initStripElements() {
-        // TODO Auto-generated method stub
-
+        Label speed = new Label("speed");
+        Button btn = new Button("btn");
+        this.getChildren().addAll(speed, btn);
     }
 
 }
