a92d6c2d21ea69716b2463155da56a0053b011f9
marco.lisotta <marco.lisotta@studio.unibo.it>
2020-04-14 15:51:27 +0200

    First instance of Tutorial Layout

---
+++ src/main/java/view/sceneController/TutorialController.java
@@ -0,0 +1,11 @@
+package view.sceneController;
+
+public class TutorialController extends AbstractSceneController implements SceneController {
+
+    /**
+     * Method that switches into menu Scenery.
+     */
+    public void switchToMenu() {
+        //TODO
+    }
+}
