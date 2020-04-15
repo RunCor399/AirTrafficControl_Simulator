9a8c5a3079476055bb403fc78a76cdc07612c2ed
marco.lisotta <marco.lisotta@studio.unibo.it>
2020-04-15 19:32:49 +0200

    Implemented first part of TutorialLayout

--- src/main/java/view/sceneController/TutorialController.java
+++ src/main/java/view/sceneController/TutorialController.java
@@ -1,6 +1,17 @@
 package view.sceneController;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class TutorialController extends AbstractSceneController implements SceneController {
+
+    @FXML
+    private Button menuButton;
+
+    @FXML
+    public final void inizialize() {
+        //TODO
+    }
 
     /**
      * Method that switches into menu Scenery.
